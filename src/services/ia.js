e46173c79784a09e3cd61e09de708694a47dd320
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-07-07 16:38:09 -0600

    Inicio del proyecto

---
+++ src/services/ia.js
@@ -0,0 +1,33 @@
+import cohere from 'cohere.-ai'
+
+cohere.init('HGC9fmSVWJwBb9qvu3ivOLZNeoFNxxIaEZzbeZng');
+
+export async function fixMyEnglish(input) {
+  const response = await cohere.generate({
+		model: 'xlarge',
+		prompt: `This is a spell checker generator
+		
+	--
+	Incorrect sample: "I is good!"
+	Correct sample: "I am good!"
+	--
+	Incorrect sample: "I have 22 years old."
+	Correct sample: "I am 22 years old."
+	--
+	Incorrect sample: "I don't can know"
+	Correct sample: "I don't know"
+	--
+		Incorrect sample: "${input}"
+		Correct sample:`,
+			max_tokens: 440,
+			temperature: 0.3,
+			k: 0,
+			p: 1,
+			frequency_penalty: 0,
+			presence_penalty: 0,
+			stop_sequences: ['--'],
+			return_likelihoods: 'NONE'
+	})
+
+	return response.body.generations[0].text
+}
