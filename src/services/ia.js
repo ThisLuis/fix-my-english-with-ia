cdb1e71bc5657944db139c02aa0fcd1ac3d5191c
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-07-07 21:15:20 -0600

    Cambiamos la manera de usar la api, no hacemos la importacion, accedemos directamente a la url del generador usando nuestra api key, hacemos un fetch pasando todos los datos necesarios y ahora ya traduce las frases erroneas por las frases correctas

--- src/services/ia.js
+++ src/services/ia.js
@@ -1,33 +1,46 @@
-import cohere from 'cohere-ai'
+const COHERE_API_KEY = 'HGC9fmSVWJwBb9qvu3ivOLZNeoFNxxIaEZzbeZng'
+const COHERE_API_GENERATE_URL = 'https://api.cohere.ai/generate'
 
-cohere.init('HGC9fmSVWJwBb9qvu3ivOLZNeoFNxxIaEZzbeZng');
 
 export async function fixMyEnglish(input) {
-  const response = await cohere.generate({
+	console.log('fixMyEnglish')
+	const data = {
 		model: 'xlarge',
 		prompt: `This is a spell checker generator
-		
-	--
-	Incorrect sample: "I is good!"
-	Correct sample: "I am good!"
-	--
-	Incorrect sample: "I have 22 years old."
-	Correct sample: "I am 22 years old."
-	--
-	Incorrect sample: "I don't can know"
-	Correct sample: "I don't know"
-	--
-		Incorrect sample: "${input}"
-		Correct sample:`,
-			max_tokens: 440,
-			temperature: 0.3,
-			k: 0,
-			p: 1,
-			frequency_penalty: 0,
-			presence_penalty: 0,
-			stop_sequences: ['--'],
-			return_likelihoods: 'NONE'
-	})
+		--
+		Incorrect sample: "I is good!"
+		Correct sample: "I am good!"
+		--
+		Incorrect sample: "I have 22 years old."
+		Correct sample: "I am 22 years old."
+		--
+		Incorrect sample: "I don't can know"
+		Correct sample: "I don't know"
+		--
+			Incorrect sample: "${input}"
+			Correct sample:`,
+				max_tokens: 40,
+				temperature: 0.3,
+				k: 0,
+				p: 1,
+				frequency_penalty: 0,
+				presence_penalty: 0,
+				stop_sequences: ['--'],
+				return_likelihoods: 'NONE'
+	}
+	const response = await fetch(COHERE_API_GENERATE_URL, {
+		method: 'POST',
+		headers: {
+			Authorization: `BEARER ${COHERE_API_KEY}`,
+			"Content-Type": 'application/json',
+			"Cohere-Version": '2022-12-06'
+		},
+		body: JSON.stringify(data)
+	}).then(res => res.json())
 
-	return response.body.generations[0].text
+	const { text } = response.generations[0]
+	return text
+		.replace('--', '')
+		.replaceAll('"', '')
+		.trim()
 }
