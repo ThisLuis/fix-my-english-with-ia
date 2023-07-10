ef7ef8bd8ff3cf470c1299e63784604a11ac72d0
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-07-09 21:42:52 -0600

    Condicionamos que solo se pueda arreglar en idioma Ingles, de lo contrario se inhabilita la opcion Fix it!

--- src/components/ButtonSubmit.svelte
+++ src/components/ButtonSubmit.svelte
@@ -21,7 +21,10 @@
 		on:click={handleClick}
 		disabled={!$isValidInput}
 		type="button" 
-		class="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+		class="{`py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ${
+			!$isValidInput ? "pointer-events-none opacity-30" : ""	
+		}`}	
+		">
 			<Upload />
 			Fix It!
 	</button>
